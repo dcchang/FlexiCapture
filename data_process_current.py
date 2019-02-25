08c0c196428c0811787c7630b48088efb435d899
David Chang <dcchang@umich.edu>
2019-02-25 16:35:49 -0500

    Arduino and post processing scripts

---
+++ data_process_current.py
@@ -0,0 +1,203 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+from tkinter import *
+from tkinter import filedialog
+import io
+#Making GUI for easy use
+
+class Main:
+	root = Tk()
+	root.title("Flex Sensor Data Processing App")
+	sd_dirname = "No directory selected"
+
+class Menu:
+	def __init__(self, master):
+		self.master = master
+		self.instruction = Label(master, text = "follow instructions below:").grid(row=0,sticky=W,columnspan=2)
+		self.instruction1 = Label(master, text = "1. Enter patient info")
+		self.instruction2 = Label(master, text = "2. Click button to upload SD card directory")
+		self.instruction3 = Label(master, text = "3. Click RUN")
+
+		self.firstname_entry = Entry(master)
+		self.firstname_label = Label(master, text = "First Name").grid(row=4,sticky=W)
+
+		self.lastname_entry = Entry(master)
+		self.lastname_label = Label(master, text = "Last Name").grid(row=5, sticky=W)
+
+		self.sd_button = Button(master,text='Upload',command = self.sd_dir)
+		self.sd_label = Label(master, text=Main.sd_dirname)
+
+		self.RunButton = Button(master,text = "RUN", fg="green", command = self.run_button, font="Helvetica 16 bold",state='disabled',anchor = CENTER)
+		self.run_label = Label(master, text = "")
+		
+		self.instruction1.grid(row=1, columnspan=2,sticky = W)
+		self.instruction2.grid(row=2,columnspan=2,sticky=W)
+		self.instruction3.grid(row=3,columnspan=2,sticky=W)
+
+		self.firstname_entry.grid(row = 4, column=1,sticky=W)
+		self.lastname_entry.grid(row=5,column=1,sticky=W)
+
+		
+		self.sd_button.grid(row=6,sticky=W)
+		self.sd_label.grid(row=6,column=1,sticky=W)
+		
+		self.RunButton.grid(row=7,columnspan=2)
+		self.run_label.grid(row=8,columnspan=2)
+
+	def check_status(self):
+		if Main.sd_dirname == "" or Main.sd_dirname == "No directory selected":
+			self.RunButton['state'] = 'disabled'
+			self.run_label['text'] = ""
+		else:
+			self.RunButton['state'] = 'normal'
+
+	def sd_dir(self):
+		Main.sd_dirname = filedialog.askdirectory(initialdir = os.getcwd(), title = "Select directory")
+		self.sd_label['text'] = Main.sd_dirname
+		if Main.sd_dirname == "":
+			self.sd_label['text'] = "No directory selected"
+		self.check_status()
+
+	def run_button(self):
+
+		try:
+			# STRAIGHT_RESISTANCE = 34
+
+			# MAX_RESISTANCE = 45
+
+			#Resistance values in kohms
+			r1 = 9.75
+			v_in = 3.7 #input voltage in V
+
+			#Read in resting voltage value:
+
+			resting_voltage_file = open(Main.sd_dirname + "/RESTING.TXT",'r')
+			resting_voltage = float(resting_voltage_file.readline())
+			resting_bend_resistance = abs((r1*resting_voltage)/(v_in-resting_voltage))
+
+			straight_voltage = 2.81
+			bend_voltage = 3.37
+
+			STRAIGHT_RESISTANCE = abs((r1*straight_voltage)/(v_in-straight_voltage))
+			MAX_RESISTANCE = abs((r1*bend_voltage)/(v_in-bend_voltage))
+
+			resting_bend_angle = np.interp(resting_bend_resistance,[STRAIGHT_RESISTANCE, MAX_RESISTANCE],[0,90.0])
+
+			df = pd.read_csv(Main.sd_dirname + "/RAW_DATA.TXT",sep='\t',skip_blank_lines = False) #filepath should point to 'raw_data.txt'
+			#read in times and voltages
+			times = df['t'] #Gets series of time values
+			times = times.tolist() #Convert series to list
+			voltages = df['V'] #Gets series of voltage values
+			voltages = voltages.tolist() #Convert series to list
+
+			#Calculate the bend resistance using known resistances and voltage values
+			#Calculate bend angles using resistance. Bend angle is relative to resting hand position.
+			#Calculate angular velocities using bend angles
+			event_times = []  #For getting duration of an event
+			durations = []
+
+			bend_angles = []
+			bend_angles_event = []
+			max_bend_angles = []
+
+
+			angular_velocities = []
+			angular_velocities_event = []
+			average_angular_velocities = []
+
+			os.makedirs("Output")
+
+			firstname = self.firstname_entry.get()
+			lastname = self.lastname_entry.get()
+
+			events_file = open("Output/%s_%s_events.txt" % (firstname,lastname),'w',encoding='utf-8')
+
+			events_header = u"t\tV\tθ\tω\n"
+			events_file.write(events_header)
+
+			x = 0
+			counter = 0
+			event_counter = 0
+			for voltage in voltages:
+				#If there is an empty line indicated by NaN, then this means the event has ended and store the max bend angle and average angular velocity
+				bend_resistance = abs((r1*voltage)/(v_in-voltage))
+				# print(bend_resistance)
+				bend_angle = np.interp(bend_resistance,[STRAIGHT_RESISTANCE, MAX_RESISTANCE],[0,90.0]) - resting_bend_angle
+				bend_angle = round(bend_angle,2)
+				bend_angles.append(bend_angle)
+
+				if counter == 0:
+					angular_velocity = 0
+					angular_velocities.append(angular_velocity)
+				else:
+					angular_velocity = ((bend_angle-bend_angles[counter-1])/(times[counter]-times[counter-1]))*1000
+					angular_velocity = round(angular_velocity,2)
+					angular_velocities.append(angular_velocity)
+
+				#Set threshold for determining an event
+				if voltage != resting_voltage:
+					#Store max bend angle for event in array
+					x = 1
+					event_times.append(times[counter])
+					bend_angles_event.append(bend_angle)
+					angular_velocities_event.append(angular_velocity)
+					event_string = str(times[counter])+"\t"+str(voltage)+"\t"+str(bend_angles_event[event_counter])+"\t"+str(angular_velocities_event[event_counter])+"\n"
+					events_file.write(event_string)
+					event_counter += 1
+				else:
+					if x == 1:
+						events_file.write("\n")
+						duration = (event_times[len(event_times)-1]-event_times[0])/1000
+						# duration = round(duration,2)
+						durations.append(duration)
+						max_bend_angles.append(max(bend_angles_event))
+						average_angular_velocities.append(np.mean(angular_velocities_event))
+						event_times = []
+						bend_angles_event = []
+						angular_velocities_event = []
+						x = 0
+						event_counter = 0
+				counter += 1
+
+			df['θ'] = bend_angles
+			df['ω'] = angular_velocities
+
+
+			#Restructure dataframe in correct column order
+			df = df[['t','V','θ','ω']]
+
+			df.to_csv('Output/%s_%s_post_processing.txt' % (firstname,lastname), sep='\t', index=False)
+
+			#Create file for processed events
+			d = {'Time (s)': durations,'Max θ': max_bend_angles,'Avg ω': average_angular_velocities}
+			df2 = pd.DataFrame(data=d)
+
+			df2 = df2[['Time (s)','Max θ', 'Avg ω']]
+			df2.to_csv('Output/%s_%s_events_processed.txt' % (firstname,lastname),sep='\t',index=False)
+
+			#Final outputs
+			total_time = (times[len(times)-1]/3600000)
+			total_event_time = (sum(durations)/3600000)
+			average_event_time = np.mean(durations)
+			average_max_angle = np.mean(max_bend_angles)
+			average_angular_velocity = np.mean(average_angular_velocities)  #Or np.mean(angular_velocities), not sure which one to use
+
+			output = open('Output/%s_%s_final_outputs.txt' % (firstname,lastname),'w')
+			output.write('Total Time (Hours) = %s\n' % total_time)
+			output.write('Total Event Time (Hours) = %s\n' % total_event_time)
+			output.write('Average Duration (sec) = %s\n' % average_event_time)
+			output.write('Average_max_angle (degrees) = %s\n' % average_max_angle)
+			output.write('Average_angular_velocity (degrees/sec) = %s\n' % average_angular_velocity)
+			output.close()
+			events_file.close()
+			self.run_label.config(text="Run complete!")
+		except Exception as e:
+			print(e)
+			print(sys.exc_info()[-1].tb_lineno)
+			self.run_label.config(text="Error!")
+#Execute code
+if __name__=="__main__":
+	Main.menu = Menu(Main.root)
+	Main.root.mainloop()
---
+++ flex_final/flex_final.ino
@@ -0,0 +1,201 @@
+//output voltage increases as flex sensor resistance increases (as it bends more)
+//calibration: for each flex sensor device, get values at 0 and 90 degrees and store them in a text file on the SD card
+//these will be read into the post processing script
+
+
+#include <ArduinoSTL.h>
+#include <SPI.h>
+#include <SD.h>
+
+//
+#if defined(ARDUINO_SAMD_ZERO) && defined(SERIAL_PORT_USBVIRTUAL)
+  // Required for Serial on Zero based boards
+  #define Serial SERIAL_PORT_USBVIRTUAL
+#endif
+//
+
+//Set date
+//date = x
+
+////declare files
+File resting_voltage_file;
+File raw_data_file;
+//declare variables
+
+//Moving average variables
+// Define the number of samples to keep track of. The higher the number, the
+// more the readings will be smoothed, but the slower the output will respond to
+// the input. Using a constant rather than a normal variable lets us use this
+// value to determine the size of the readings array.
+const int numReadings = 10;
+
+int readings[numReadings];      // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+int total = 0;                  // the running total
+int average = 0;                // the average
+
+
+
+
+//Voltage values
+int resting_flexADC;
+int resting_flexADC_average;
+int flexADC;
+float resting_flexV;
+
+const int chipSelect = 10;
+const int FLEX_PIN = A1; // Pin connected to voltage divider output
+
+// Measure the voltage at 5V and the actual resistance of your
+// 47k resistor, and enter them below:
+const float VCC = 3.7; // Measured voltage of Ardunio 5V line
+//const float R_DIV = 9750.0; // Measured resistance of 3.3k resistor
+
+// Upload the code, then try to adjust these values to more
+// accurately calculate bend degree.
+//const float STRAIGHT_RESISTANCE = 23000; // resistance when straight
+//const float BEND_RESISTANCE = 44800.0; // resistance at 90 deg
+float flexR;
+float flexR_0;
+float flexR_90;
+float resistance_5;
+float voltage_5;
+float voltage_diff;
+
+//time variables
+unsigned long start_time;
+unsigned long end_time;
+//unsigned long finish;
+
+//data structure for storing information that will be written to SD card
+struct Data {
+
+  unsigned long current_time;
+  float flexV;
+
+};
+
+Data vector_item; 
+
+std::vector<Data> data_vector; 
+//switch for determining whether just in while loop or not
+int x = 0;
+
+//do this everytime you put the device back on, so turn off and back on device every time you take it off
+//Create resting voltage file and new data file every time you turn device back on
+void setup() {
+  //Battery Monitor
+  pinMode(8,OUTPUT);
+//  pinMode(7,OUTPUT);
+//  pinMode(12,OUTPUT);
+//
+//  digitalWrite(8,HIGH);
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  pinMode(10, OUTPUT);
+  pinMode(FLEX_PIN, INPUT);
+  
+  if (!SD.begin(SPI_HALF_SPEED,10)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+
+  //get current time and date
+  //date = getDate()
+  //time = getTime()
+  //File names change for each day
+  //This way, data for each day can be in their own folder
+  //When you do post processing, you can click one folder at a time 
+  //cannot turn off board, instead make it sleep
+  String data_filename = String(date) + String(Time) + "_rawdata.txt";
+  raw_data_file = SD.open(filename, FILE_WRITE);
+  
+  raw_data_file.println("t\tV");
+  
+  for (int i=0; i<3000; i++) {
+    resting_flexADC = analogRead(FLEX_PIN);
+    total = total + resting_flexADC;
+    delay(1);
+  }
+  resting_flexADC_average = total/3000;
+  
+  resting_flexV = resting_flexADC_average * VCC/ 1023.0;  //Calculate voltage at resting position
+  
+  resting_voltage_file = SD.open("resting.txt", FILE_WRITE);
+  resting_voltage_file.println(resting_flexV); //Stores the resting voltage value onto a txt file
+  
+  Serial.println("Resting voltage: " + String(resting_flexV));
+  
+  // initialize all the readings to resting_flexADC:
+  total = 0;
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = resting_flexADC_average;
+    total += readings[thisReading];
+  }
+  start_time = millis();
+
+  raw_data_file.close();
+  resting_voltage_file.close();
+  
+}
+
+void loop() {
+  
+  float batterylevel = analogRead(A3);
+//  Serial.println("BatteryVoltage = ");
+//  Serial.println(batterylevel*VCC/1023);
+  if ((batterylevel*VCC/1023) < VCC) {
+    digitalWrite(8,LOW);
+//    digitalWrite(7,HIGH);
+  }
+  else {
+    digitalWrite(8,HIGH);
+//    digitalWrite(7,LOW);
+//  
+  }
+
+  ///Calculate moving average 
+  // subtract the last reading:
+  total = total - readings[readIndex];
+  // read from the sensor:
+  readings[readIndex] = analogRead(FLEX_PIN);
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / numReadings;
+
+  //Calculate voltage
+  vector_item.flexV = average * VCC / 1023.0; //calculate voltage
+  
+  end_time = millis();
+
+  vector_item.current_time = end_time - start_time;
+  
+  data_vector.push_back({vector_item.current_time,vector_item.flexV});
+  Serial.println("Time: " + String(vector_item.current_time) + "\t" + "Voltage Value: " + String(vector_item.flexV));  
+ 
+  if (data_vector.size() >= 100) {
+    raw_data_file = SD.open("raw_data.txt", FILE_WRITE);
+    for (int i=0; i< data_vector.size(); i++) {
+      raw_data_file.println(String(data_vector[i].current_time) + "\t" + String(data_vector[i].flexV));
+      Serial.println("Writing to SD Card");
+    }
+    raw_data_file.flush();
+    data_vector.clear();
+    raw_data_file.close();
+  } 
+  delay(25); //measure data at intervals of 25 milliseconds
+}
