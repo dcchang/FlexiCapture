6c3bb4ff83b4d417c25d3e19bad527c80b01a7b0
David Chang <dcchang@umich.edu>
2019-04-22 00:49:04 -0400

    Update bend_angle.py

--- Post Processing Code/bend_angle.py
+++ Post Processing Code/bend_angle.py
@@ -3,9 +3,8 @@
 import numpy as np
 
 #User input
-r_0 = 12.93867925  #resistance value at 0 degrees
-r_90 = 18.21511628 #resistance value at 90 degrees
-
+r_0 = 10.51685393  #resistance value at 0 degrees
+r_90 = 17.78816794 #resistance value at 90 degrees
 
 input_file = open("acc_data.txt", "r")
 output = open("acc_results.txt", "w")
@@ -15,7 +14,7 @@ data = list(map(float,data))			#Converts all values in list to floats
 
 for item in data: 
 	#Uses one-dimensional linear interpolation given resistance at 0 and 90 degrees to estimate bend angle
-	angle = np.interp(item,[12.93867925,18.21511628],[0,90])	
+	angle = np.interp(item,[r_0,r_90],[0,90])	
 	print(angle)
 	output.write(str(angle))	#Saves bend angles to file
 	output.write('\n')
